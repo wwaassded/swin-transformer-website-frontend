eab40e8dd3995f536f3fd89a2b20eea8a100e217
wwaassded <wwssd507@gmail.com>
2025-02-06 11:46:23 +0800

    now you can trust backend celery

--- src/store/websocket.ts
+++ src/store/websocket.ts
@@ -4,7 +4,9 @@ import {ref} from "vue";
 export const useWebSocketStore = defineStore('websocket', () => {
     const ws = ref()
     const status = ref('disconnected')
-    const result = ref('')
+    const result = ref({})
+    const isSuccessful = ref(false)
+    const isCompleted = ref(false) // 页面只需要根据这个变量来进行分支渲染即可
     const connect = (taskId: string) => {
         if (ws.value) {
             console.log('websocket connection has already been established')
@@ -14,11 +16,21 @@ export const useWebSocketStore = defineStore('websocket', () => {
         ws.value.onopen = () => {
             status.value = 'connected'
             console.log('websocket connection established')
+            console.log(`my task_id is:${taskId}`)
         }
         ws.value.onmessage = (event: MessageEvent) => {
-            console.log('********')
-            console.log(event)
-            console.log('********')
+            isCompleted.value = true
+            const received_data = event.data
+            if (received_data.isSuccessful) {
+                isSuccessful.value = true
+                result.value = received_data
+            } else {
+                isSuccessful.value = false
+                result.value = {
+                    'isSuccessful': false,
+                    'message': 'failed to handle your image'
+                }
+            }
         }
         ws.value.onerror = (e: Error) => {
             console.error('websocket error: ', e)
